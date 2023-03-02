b8b96a037bfecaa9837cc07151255b87138cae00
Abdul Aziz Ahwan <abdulazizahwan97@gmail.com>
2023-03-02 12:00:26 +0700

    try again

--- next.config.js
+++ next.config.js
@@ -17,6 +17,11 @@ module.exports = {
             'lh5.googleusercontent.com',
             'lh6.googleusercontent.com'
         ],
+        deviceSizes: [320, 420, 768, 1024, 1200],
+        path: '/_next/image',
+        loader: 'imgix',
+        imageSizes: [16, 32, 48, 64, 96],
+        minimumCacheTTL: 60,
     },
 };
 
